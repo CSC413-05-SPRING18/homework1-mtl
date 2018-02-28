0b4a82fda95cca6b1a689a5577df3cf04fa4c9cc
Peter Lin <peterlin.california@gmail.com>
2018-02-28 13:16:06 -0800

    finished the second part of post

--- src/Processor/PostProcessor.java
+++ src/Processor/PostProcessor.java
@@ -14,29 +14,26 @@ public class PostProcessor extends Processor {
 
     @Override
     public String process() {
-        ResponseBuilder responseBuilder = new ResponseBuilder();
-
-
-        //this function splits it up and does stuff if theres a post ID
-        String[] argParts = this.args.split("=");               //splits up the argument parts
+        ResponseBuilder responseBuilder = new ResponseBuilder();                       //this function splits it up and does stuff if theres a post ID
+        String[] argParts = this.args.split("=");                                //splits up the argument parts
         String argName = argParts[0];
-        //if (argName == "postid") {                                      //if argName is postid, search by using the post's ID
-        int postid = Integer.parseInt(argParts[1]);                //converts string to int
-        Post post = Post.getPostByPostID(postid);
-        responseBuilder.setStatus("OK");
-        responseBuilder.setData(post);
-
-
-        if (argName == "userid") {                                  //if argName is userid, search up by user's ID
-            int userid = Integer.parseInt(argParts[1]);
-            post = Post.getPostByUser(userid);
+        if (argName.equals("postid")) {                                                     //if argName is postid, search by using the post's ID
+            int postid = Integer.parseInt(argParts[1]);                                //converts string to int
+            Post post = Post.getPostByPostID(postid);
             responseBuilder.setStatus("OK");
             responseBuilder.setData(post);
         }
-        Gson gson = new Gson();
-        Response response = responseBuilder.build();
 
-        return gson.toJson(response);
+        if (argName.equals("userid")) {                                                     //if argName is userid, search up by user's ID
+            int userid = Integer.parseInt(argParts[1]);
+            Post post = Post.getPostByUser(userid);
+            responseBuilder.setStatus("OK");
+            responseBuilder.setData(post);
+        }
+            Gson gson = new Gson();
+            Response response = responseBuilder.build();
 
+            return gson.toJson(response);
+
+        }
     }
-}
